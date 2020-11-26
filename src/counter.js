0cfe98063d654927457a04571c25e7c3b86760b5
ykpark <read0more@gmail.com>
2020-11-26 16:51:30 +0900

    counter.spec.js 작성

--- src/counter.js
+++ src/counter.js
@@ -1,13 +1,22 @@
-export function createCounter() {
+export function createCounter(initObject = {}) {
+  let value = initObject.value ?? 0;
+  const { min, max } = initObject;
+
   return {
     val() {
-
+      return value;
     },
     inc() {
-
+      !this.isMax() && value++;
     },
     dec() {
-
-    }
+      !this.isMin() && value--;
+    },
+    isMax() {
+      return !!max && value <= max;
+    },
+    isMin() {
+      return !!min && value <= min;
+    },
   };
 }
