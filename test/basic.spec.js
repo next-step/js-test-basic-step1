ecc0e9feced8938b3e322d488212e0866b973886
eyabc <bey4314@naver.com>
2020-11-26 23:20:29 +0900

    feat: Array.prototype.slice 테스트

--- test/basic.spec.js
+++ test/basic.spec.js
@@ -51,18 +51,18 @@ describe('Array.prototype.slice()', () => {
   const nums = [1, 2, 3, 4, 5];
 
   it('인자가 없으면 전체 배열을 복사해서 그대로 반환한다.', () => {
-
+    expect(nums.slice()).toEqual([1, 2, 3, 4, 5]);
   });
 
   it('인자가 하나이면, 주어진 인덱스부터 마지막 인덱스까지의 배열을 반환한다.', () => {
-
+    expect(nums.slice(3)).toEqual([4, 5]);
   });
 
   it('인자가 두 개이면, 인덱스가 첫 번째 인자와 두 번째 인자 사이에 해당하는 배열을 반환한다.', () => {
-
+    expect(nums.slice(1, 3)).toEqual([2, 3]);
   });
 
   it('반환되는 배열은 기존 배열과 다른 새로운 배열이다.', () => {
-
+    expect(nums.slice()).not.toBe(nums);
   });
 });
