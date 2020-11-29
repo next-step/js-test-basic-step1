a10ca2cffcc6d7ad3852c1a2a815b25825a5ca34
jho2301 <jho2301@gmail.com>
2020-11-29 15:55:28 +0900

    feat: add기능 구현

--- src/util.js
+++ src/util.js
@@ -1,7 +1,12 @@
-export function add() {
+export function add(a, b) {
+  if (a === undefined && b === undefined) {
+    return 0;
+  }
+  if (b === undefined) {
+    return a;
+  }
 
+  return a + b;
 }
 
-export function swap() {
-
-}
+export function swap() {}
