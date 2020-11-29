ba4b4d2794df7a3fbd3dc9b6d751f66ba3f4c463
jho2301 <jho2301@gmail.com>
2020-11-29 16:06:06 +0900

    feat: swap 기능 구현

--- src/util.js
+++ src/util.js
@@ -9,4 +9,9 @@ export function add(a, b) {
   return a + b;
 }
 
-export function swap() {}
+export function swap(array) {
+  if (array.length !== 2) {
+    return array;
+  }
+  return [array[1], array[0]];
+}
