55343298cc025f170b3e1906fc7baa25ff7b04cf
jho2301 <jho2301@gmail.com>
2020-11-29 15:10:44 +0900

    test: counter.spec, step2테스트코드 작성

--- test/counter.spec.js
+++ test/counter.spec.js
@@ -34,17 +34,37 @@ describe("step 1", () => {
 
 describe("step2", () => {
   // Step 2
-  it("initValue 옵션 사용 시 초기값이 해당 값으로 지정된다.", () => {});
+  it("initValue 옵션 사용 시 초기값이 해당 값으로 지정된다.", () => {
+    const counter = createCounter({ initValue: 10 });
+    expect(counter.val()).toBe(10);
+    expect(counter.val()).not.toBe(0);
+  });
 
   describe("min 옵션 사용 시 현재값과 min 값이 동일하면", () => {
-    it("dec() 함수를 호출해도 값이 감소하지 않는다.", () => {});
+    const MIN_VALUE = 0;
+    const counter = createCounter({ initValue: MIN_VALUE, min: MIN_VALUE });
+    it("dec() 함수를 호출해도 값이 감소하지 않는다.", () => {
+      counter.dec();
+      expect(counter.val()).toBe(MIN_VALUE);
+      expect(counter.val()).not.toBe(MIN_VALUE - 1);
+    });
 
-    it("isMin() 호출 시 true를 반환한다.", () => {});
+    it("isMin() 호출 시 true를 반환한다.", () => {
+      expect(counter.isMin()).toBe(true);
+    });
   });
 
   describe("max 옵션 사용 시 현재값과 max 값이 동일하면", () => {
-    it("inc() 함수를 호출해도 값이 증가하지 않는다.", () => {});
+    const MAX_VALUE = 0;
+    const counter = createCounter({ initValue: MAX_VALUE, max: MAX_VALUE });
+    it("inc() 함수를 호출해도 값이 증가하지 않는다.", () => {
+      counter.inc();
+      expect(counter.val()).toBe(MAX_VALUE);
+      expect(counter.val()).not.toBe(MIN_VALUE + 1);
+    });
 
-    it("isMax() 호출 시 true를 반환한다.", () => {});
+    it("isMax() 호출 시 true를 반환한다.", () => {
+      expect(counter.isMax()).toBe(true);
+    });
   });
 });
