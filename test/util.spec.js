2ab18b0d616e040a0729727b682a419add3ae4d0
jho2301 <jho2301@gmail.com>
2020-11-29 16:05:44 +0900

    test: util.spec, swap함수 테스트코드 작성

--- test/util.spec.js
+++ test/util.spec.js
@@ -21,9 +21,25 @@ describe("add()", () => {
 });
 
 describe("swap()", () => {
-  test("배열의 인자가 두 개가 아닌 경우, 기존 배열을 그대로 반환한다.", () => {});
+  const arrayWithLength1 = [1];
+  const arrayWithLength2 = [1, 2];
+  const swappedArrayWithLength2 = [2, 1];
+  const arrayWithLength3 = [1, 2, 3];
 
-  test("배열 내의 두 요소의 순서를 바꾸어 새로운 배열을 반환한다.", () => {});
+  test("배열의 인자가 두 개가 아닌 경우, 기존 배열을 그대로 반환한다.", () => {
+    expect(swap(arrayWithLength1)).toBe(arrayWithLength1);
+    expect(swap(arrayWithLength1)).toEqual(arrayWithLength1);
+    expect(swap(arrayWithLength3)).toBe(arrayWithLength3);
+    expect(swap(arrayWithLength3)).toEqual(arrayWithLength3);
+    expect(swap(arrayWithLength2)).not.toBe(arrayWithLength2);
+    expect(swap(arrayWithLength2)).not.toEqual(arrayWithLength2);
+  });
 
-  test("변경된 배열은 기존 배열과 다른 새로운 배열이다.", () => {});
+  test("배열 내의 두 요소의 순서를 바꾸어 새로운 배열을 반환한다.", () => {
+    expect(swap(arrayWithLength2)).toEqual(swappedArrayWithLength2);
+  });
+
+  test("변경된 배열은 기존 배열과 다른 새로운 배열이다.", () => {
+    expect(swap(arrayWithLength2)).not.toBe(arrayWithLength2);
+  });
 });
