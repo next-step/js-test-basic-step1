343db7bf9de1045c11dd9a38c4417791ed7a5403
younho9 <younho9.choo@gmail.com>
2020-12-02 02:14:32 +0900

    feat: Complete basic test

--- test/basic.spec.js
+++ test/basic.spec.js
@@ -37,19 +37,58 @@ describe('toBe / toEqual', () => {
 });
 
 describe('Math.round()', () => {
-  it('소숫점 이하 값이 0.5 이상이면 올림 값을 반환한다', () => {});
+  it('소숫점 이하 값이 0.5 이상이면 올림 값을 반환한다', () => {
+    expect(Math.round(0.6)).toEqual(1);
+    expect(Math.round(0.7)).toEqual(1);
+    expect(Math.round(0.8)).toEqual(1);
+    expect(Math.round(0.9)).toEqual(1);
+  });
 
-  it('소숫점 이하 값이 0.5 미만이면 내림 값을 반환한다', () => {});
+  it('소숫점 이하 값이 0.5 미만이면 내림 값을 반환한다', () => {
+    expect(Math.round(0.4)).toEqual(0);
+    expect(Math.round(0.3)).toEqual(0);
+    expect(Math.round(0.2)).toEqual(0);
+    expect(Math.round(0.1)).toEqual(0);
+  });
 });
 
 describe('Array.prototype.slice()', () => {
   const nums = [1, 2, 3, 4, 5];
 
-  it('인자가 없으면 전체 배열을 복사해서 그대로 반환한다.', () => {});
+  it('인자가 없으면 전체 배열을 복사해서 그대로 반환한다.', () => {
+    expect(nums.slice()).toEqual(nums);
+  });
 
-  it('인자가 하나이면, 주어진 인덱스부터 마지막 인덱스까지의 배열을 반환한다.', () => {});
+  it('인자가 하나이면, 주어진 인덱스부터 마지막 인덱스까지의 배열을 반환한다.', () => {
+    expect(nums.slice(0)).toEqual([nums[0], nums[1], nums[2], nums[3],nums[4]]); // prettier-ignore
+    expect(nums.slice(1)).toEqual([nums[1], nums[2], nums[3], nums[4]]);
+    expect(nums.slice(2)).toEqual([nums[2], nums[3], nums[4]]);
+    expect(nums.slice(3)).toEqual([nums[3], nums[4]]);
+    expect(nums.slice(4)).toEqual([nums[4]]);
+  });
 
-  it('인자가 두 개이면, 인덱스가 첫 번째 인자와 두 번째 인자 사이에 해당하는 배열을 반환한다.', () => {});
+  it('인자가 두 개이면, 인덱스가 첫 번째 인자와 두 번째 인자 사이에 해당하는 배열을 반환한다.', () => {
+    expect(nums.slice(0, 0)).toEqual([]);
+    expect(nums.slice(0, 1)).toEqual([nums[0]]);
+    expect(nums.slice(0, 2)).toEqual([nums[0], nums[1]]);
+    expect(nums.slice(0, 3)).toEqual([nums[0], nums[1], nums[2]]);
+    expect(nums.slice(0, 4)).toEqual([nums[0], nums[1], nums[2], nums[3]]);
+    expect(nums.slice(0, 5)).toEqual([nums[0], nums[1], nums[2], nums[3], nums[4]]); // prettier-ignore
+    expect(nums.slice(1, 5)).toEqual([nums[1], nums[2], nums[3], nums[4]]);
+    expect(nums.slice(2, 5)).toEqual([nums[2], nums[3], nums[4]]);
+    expect(nums.slice(3, 5)).toEqual([nums[3], nums[4]]);
+    expect(nums.slice(4, 5)).toEqual([nums[4]]);
+    expect(nums.slice(5, 5)).toEqual([]);
+    expect(nums.slice(-1, 0)).toEqual([]);
+    expect(nums.slice(1, 1)).toEqual([]);
+    expect(nums.slice(2, 2)).toEqual([]);
+    expect(nums.slice(3, 3)).toEqual([]);
+    expect(nums.slice(4, 4)).toEqual([]);
+    expect(nums.slice(5, 5)).toEqual([]);
+    expect(nums.slice(5, 6)).toEqual([]);
+  });
 
-  it('반환되는 배열은 기존 배열과 다른 새로운 배열이다.', () => {});
+  it('반환되는 배열은 기존 배열과 다른 새로운 배열이다.', () => {
+    expect(nums.slice()).not.toBe(nums);
+  });
 });
